c7ef5c4b4dadc4e8077f845d2a6873ad8af348c4
Sam Reed <sam.rich.reed@berkeley.edu>
2017-11-27 09:18:02 -0800

    Getting the post to work

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root 'users#new'
-	get './users/new', to: 'users#new'
-    post './users', to: 'users#create'
+	get 'fa17_2017/fa17-proj2/users/new', to: 'users#new'
+    post 'fa17_2017/fa17-proj2/users', to: 'users#create'
 	resources :users
 
 end
