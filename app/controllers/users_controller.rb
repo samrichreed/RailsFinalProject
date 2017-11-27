6fb6164d7799fd63fcf25e4eaaf03a4dfacf25b2
Sam Reed <sam.rich.reed@berkeley.edu>
2017-11-27 04:57:32 -0800

    Some changes to the home screen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
+
+  def index
+  	@users = Users.all
+  end
+
+  def show
+  end
+
   def new
   end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
