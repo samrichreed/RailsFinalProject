d5ed306d8dfa0ccb945214407ff710e19a5d1089
Sam Reed <sam.rich.reed@berkeley.edu>
2017-11-27 03:44:45 -0800

    revert back

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get '/' to: 'ratings#new'
+root to: 'ratings#new'
 get '/ratings/new', to: 'ratings#new'
 post '/ratings', to: 'ratings#create'
 
