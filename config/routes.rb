9eba495697aab510891bbab0cf90bb1c08e49d0b
Sam Reed <sam.rich.reed@berkeley.edu>
2017-11-27 03:08:13 -0800

    Fixed bad route 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: 'ratings#new'
-get '/fa17-proj2/ratings/new', to: 'ratings#new'
+get 'ratings/new', to: 'ratings#new'
 post '/fa17-proj2/ratings', to: 'ratings#create'
 
 end
