5e68dee96b68e1b688b4b432e04fbb5e64ef9d83
Sam Reed <sam.rich.reed@berkeley.edu>
2017-11-27 09:42:09 -0800

    Getting the post to work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def save
+  end
+
   private
 	def user_params
 	  params.require(:user).permit(:name, :email, :password,
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	root 'users#new'
-	get '/users/new', to: 'users#new'
-    post '/users', to: 'users#create'
+	root 'pages#home'
+    get    '/signup',  to: 'users#new'
+    get    '/login',   to: 'sessions#new'
+    post   '/login',   to: 'sessions#create'
+    delete '/logout',  to: 'sessions#destroy'
 	resources :users
 	
 end
