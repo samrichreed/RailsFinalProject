bee41b8315f2f8b7715bb3fc2d7447b4d9b2e989
Sam Reed <sam.rich.reed@gmail.com>
2017-11-27 11:47:28 +0000

    Added users model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: 'ratings#new'
 get '/ratings/new', to: 'ratings#new'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171116000141) do
+ActiveRecord::Schema.define(version: 20171127114505) do
 
   create_table "ratings", force: :cascade do |t|
     t.integer "stars"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20171116000141) do
   create_table "songs", force: :cascade do |t|
     t.string "name"
     t.string "artist"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
