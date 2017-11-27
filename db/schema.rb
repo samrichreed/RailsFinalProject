06d8f0cf08a1c53589e6c4286833c6323c8ace89
Sam Reed <sam.rich.reed@gmail.com>
2017-11-27 12:17:40 +0000

    Migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127120837) do
+ActiveRecord::Schema.define(version: 20171127121531) do
 
   create_table "ratings", force: :cascade do |t|
     t.integer "stars"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20171127120837) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
