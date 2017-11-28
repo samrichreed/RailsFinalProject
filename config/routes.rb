0a550b285a5075eb034124c89d9ee9db3e354907
Sam Reed <sam.rich.reed@berkeley.edu>
2017-11-27 20:00:32 -0800

    Land on a new session

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	root 'session#new'
+	root 'sessions#new'
     get    '/signup',  to: 'users#new'
     get    '/login',   to: 'sessions#new'
     post   '/login',   to: 'sessions#create'
